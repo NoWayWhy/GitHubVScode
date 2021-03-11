ad58b29b75da26ffff96eda49ce20b29f93da1ec
NoWayWhy <79251995+NoWayWhy@users.noreply.github.com>
2021-03-11 17:55:49 +0700

    Edit Window2

--- Simple_drawing_window2.py
+++ Simple_drawing_window2.py
@@ -15,20 +15,22 @@ class Simple_drawing_window(QWidget):
 
         p.setPen(QColor(0,0,0))
         p.setBrush(QColor(0,127,0))
-        p.drawPolygon([
-            QPoint(70,100),QPoint(100,110),
-            QPoint(130,100),QPoint(100,150),
-        ])
 
-        p.setPen(QColor(255,127,0))
+        p.setPen(QColor(255,255,125))
+        p.setBrush(QColor(255,255,0))
+        p.drawPie(50,150,100,100,0,180*27)
+
+        p.setBrush(QColor(0,0,0))
+        p.drawPie(90,170,20,20,0,180*32)
+
         p.setBrush(QColor(255,127,0))
-        p.drawPie(50,150,100,100,0,180*16)
-        
-        p.drawPolygon(
-            [QPoint(50,200),QPoint(150,200),QPoint(100,400),]
-        )
+        p.drawPie(160,230,20,20,0,180*32)
 
-        p.drawPixmap(QRect(200,100,320,320),self.rabbit)
+        p.drawPie(270,310,20,20,0,180*32)
+
+        p.drawPie(380,390,20,20,0,180*32)
+
+        p.drawPixmap(QRect(300,60,200,200),self.rabbit)
         p.end()
 
 def main():
