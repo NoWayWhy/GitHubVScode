cef3be54fa8de33db61994cd891f7652194b3f50
TunaCorn <79251337+TunaCorn@users.noreply.github.com>
2021-03-11 18:00:22 +0700

    Update simple_drawing_window3.py

--- simple_drawing_window3.py
+++ simple_drawing_window3.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window3(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Drawing")
@@ -14,19 +14,13 @@ class Simple_drawing_window(QWidget):
         p.begin(self)
 
         p.setPen(QColor(0,0,0))
-        p.setBrush(QColor(0,127,0))
-        p.drawPolygon([
-            QPoint(70,100),QPoint(100,110),
-            Qpoint(130,100)<QPoint(100,150),
-        ])
+        p.setBrush(QColor(255,255,0))
+        p.drawPie(50,150,100,100,0,180*32)
 
-        p.setPen(QColor(255,127,0))
-        p.setBrush(QColor(255,127,0))
-        p.drawPie(50,150,100,100,0,180*16)
-        
-        p.drawPolygon(
-            [QPoint(50,200),QPoint(150,200),QPoint(100,400),]
-        )
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,0,0))
+        p.drawPie(50,150,100,100,0,90*32)
+
 
         p.drawPixmap(QRect(200,100,320,320),self.rabbit)
         p.end()
